1edbb996a217c26ed72d9205477626d2191d81b2
vaishnavi_in <vaishnavi.in@robosoftin.com>
2020-09-12 12:49:50 +0530

    Implementing Strings and unary operator

--- chapter-01/1.js
+++ chapter-01/1.js
@@ -1,5 +1,9 @@
 console.log(100 + 4 * 11)
+
 console.log((100 + 4) * 11)
+
 console.log(4 - 1 + 2)
+
 console.log(4 - (1 + 2))
+
 console.log(207 % 10)
---
+++ chapter-01/2.js
@@ -0,0 +1,8 @@
+console.log("An example string")
+
+console.log("This is the first line\nAnd this is the second line")
+
+console.log("A new line character is written like \"\\n\".")
+
+console.log(typeof 5)  //Unary operator
+console.log(typeof "a")  //Unary operator

5a35c26374edfb70ef968318de8c1b0101732605
vaishnavi_in <vaishnavi.in@robosoftin.com>
2020-09-12 14:38:58 +0530

    Implementing automatic type conversions and short circuiting

--- chapter-01/2.js
+++ chapter-01/2.js
@@ -4,5 +4,5 @@ console.log("This is the first line\nAnd this is the second line")
 
 console.log("A new line character is written like \"\\n\".")
 
-console.log(typeof 5)  //Unary operator
-console.log(typeof "a")  //Unary operator
+console.log(typeof 5)  // Unary operator
+console.log(typeof "a")  // Unary operator
---
+++ chapter-01/5.js
@@ -0,0 +1,13 @@
+console.log(8 * null)
+
+console.log("5" - 1)
+
+console.log("5" + 1)
+
+console.log("five" * 2)
+
+console.log(false == 0)
+
+console.log(null || "user")  // Short circuiting
+
+console.log("Agnes" || "user")  // Short circuiting

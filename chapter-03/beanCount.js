c0c1bdda0a9ad00fe16b5c50a03338f90175df81
vaishnavi_in <vaishnavi.in@robosoftin.com>
2020-09-16 20:35:08 +0530

    Add the Bean Counting program

---
+++ chapter-03/beanCount.js
@@ -0,0 +1,12 @@
+"use strict";
+function countChar(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] === letter) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(countChar("kakkerklake", "k"));

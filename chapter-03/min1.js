119ba1e58714fa678e75d358f92200cf9b48864e
vaishnavi_in <vaishnavi.in@robosoftin.com>
2020-09-18 17:32:29 +0530

    Revert "Adding test cases to min function and modified beanCount"
    This reverts commit 33b0aee8063de63a5acb02083da4d4cadc9a13f3.

--- chapter-03/min1.js
+++ chapter-03/min1.js
@@ -3,18 +3,4 @@ function min(a, b) {
     return a < b ? a : b; 
 }
 
-function test(label, body) {
-    if(!body()) console.log(`Failed: ${label}`);
-}
-
-test("Minimum of (1, 10) returns 1", () => {
-    return min(1, 10) === 1;
-});
-
-test("Minimum of (2, 9) returns 9", () => {
-    return min(2, 9) === 9;
-});
-
-test("Minimum of (0, -1) returns -1", () => {
-    return min(0, -1) === -1;
-});
+console.log(min(2, -1));

ee7257ac8e387bdc71165a67e324084642c46fe2
vaishnavi_in <vaishnavi.in@robosoftin.com>
2020-09-18 18:18:15 +0530

    Added test cases to minimum function

--- chapter-03/min1.js
+++ chapter-03/min1.js
@@ -3,4 +3,18 @@ function min(a, b) {
     return a < b ? a : b; 
 }
 
-console.log(min(2, -1));
+function test(label, body) {
+    if(!body()) return console.log(`Failed: ${label}`);
+}
+
+test("Minimum of (1, 10) returns 1", () => {
+    return min(1, 10) === 1;
+});
+
+test("Minimum of (2, 9) returns 9", () => {
+    return min(2, 9) === 9;
+});
+
+test("Minimum of (2, -1) returns -1", () => {
+    return min(2, -1) === -1;
+});
